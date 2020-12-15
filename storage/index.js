7351b3ddf608e3768fea825d7223dfb533eea26a
zhuqingsong <735560190@qq.com>
2020-12-15 21:50:28 +0800

    fix: undefined is a variable rather then a string

--- storage/index.js
+++ storage/index.js
@@ -2,7 +2,7 @@ const storage = {
     set(key, value, expires) {
         localStorage[key] = JSON.stringify({
             value,
-            expires: expires === 'undefined' ? undefined : Date.now() + 1000 * expires
+            expires: expires === undefined ? undefined : Date.now() + 1000 * expires
         })
     },
     get(key) {
